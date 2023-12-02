1265ffa38935940f7d54cf56fb89a4863423cd39
Shamir León <leonshamir1@gmail.com>
2023-12-02 00:27:19 -0500

    Add component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
+import data from '@/data/data.json'
+import CommentCard from './components/CommentCard.vue'
 
+const user = data.comments[0]
 </script>
 
 <template>
-  <h1>Hello World with Vue3</h1>
+  <main class="bg-Backgroun w-[100vw] h-[100vh] px-32">
+    <CommentCard :data="user"></CommentCard>
+  </main>
 </template>
-
-<style scoped>
-
-</style>
