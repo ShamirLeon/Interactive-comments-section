f2306310d9ff56259797d3fd4b726b6a79477857
Shamir León <leonshamir1@gmail.com>
2023-12-02 00:26:51 -0500

    Add tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        /* Primary */
+        'Moderate-Blue': '#5457b6',
+        'Soft-Red': '#ed6468',
+        'LightGrayish-Blue': '#c3c4ef',
+        'Pale-Red': '#ffb8bb',
+
+        /* Neutral */
+        'Dark-Blue': '#324152',
+        'Grayish-Blue': '#67727e',
+        'Light-Gray': '#ebedf2',
+        'Very-Light-Gray': '#f5f6fa',
+        "Backgroun": '#f5f7fb'
+      },
+
+      fontFamily: {
+        Rubik: ['Rubik']
+      }
+    }
   },
   plugins: []
 }
