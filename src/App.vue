e1964846800930b51d697fda6105323f91645889
Shamir León <leonshamir1@gmail.com>
2023-12-02 23:05:15 -0500

    Finish card styles

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,12 @@
 import data from '@/data/data.json'
 import CommentCard from './components/CommentCard.vue'
 
-const user = data.comments[0]
 </script>
 
 <template>
-  <main class="bg-Backgroun w-[100vw] h-[100vh] px-32">
-    <CommentCard :data="user"></CommentCard>
+  <main class="bg-Backgroun w-[100vw] h-[100vh] px-32 pt-16 flex flex-col items-center">
+    <div v-for="(comment, index) in data.comments" key="index">
+      <CommentCard :data="comment"></CommentCard>
+    </div>
   </main>
 </template>
--- src/components/CommentCard.vue
+++ src/components/CommentCard.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-defineProps<{
+import { ref } from 'vue';
+const props = defineProps<{
   data: {
     id: number
     content: string
@@ -14,23 +15,35 @@ defineProps<{
     }
   }
 }>()
+
+let score =  ref(props.data.score)
+
+const increaseScore = () => score.value++; 
+const decreaseScore = () => score.value-- ;
 </script>
 
 <template>
-  <div class="bg-white p-8 rounded-md">
-    <div class="flex justify-between mb-4">
-      <div class="flex gap-4 items-center">
-        <img class="w-10 h-10" :src="data.user.image.png" :alt="data.user.username" />
-        <h2 class="text-Dark-Blue font-bold">{{ data.user.username }}</h2>
-        <span class="text-Grayish-Blue">{{ data.createdAt }}</span>
-      </div>
-      <div class="flex gap-2 items-center">
-        <img src="./assets/images/icon-reply.svg" alt="" />
-        <span class="text-Moderate-Blue font-bold">Reply</span>
-      </div>
+  <div class="bg-white p-8 rounded-md flex gap-8 max-w-3xl mb-4">
+    <div class="flex flex-col items-center justify-center">
+      <button @click="increaseScore" class="grid place-content-center rounded-t-lg bg-Light-Gray w-8 h-8"><img src="../assets/images/icon-plus.svg" alt=""></button>
+      <span class="w-8 h-8 text-center bg-Light-Gray font-bold text-Moderate-Blue">{{ score }}</span>
+      <button @click="decreaseScore" class="grid place-content-center rounded-b-lg bg-Light-Gray w-8 h-8"><img src="../assets/images/icon-minus.svg" alt=""></button>
     </div>
     <div>
-      <p class="text-Grayish-Blue">{{ data.content }}</p>
+      <div class="flex justify-between mb-4">
+        <div class="flex gap-4 items-center">
+          <img class="w-10 h-10" :src="data.user.image.png" :alt="data.user.username" />
+          <h2 class="text-Dark-Blue font-bold">{{ data.user.username }}</h2>
+          <span class="text-Grayish-Blue">{{ data.createdAt }}</span>
+        </div>
+        <button @click="increaseScore" class="flex gap-2 items-center">
+          <img src="../assets/images/icon-reply.svg" />
+          <span class="text-Moderate-Blue font-bold">Reply</span>
+        </button>
+      </div>
+      <div>
+        <p class="text-Grayish-Blue">{{ data.content }}</p>
+      </div>
     </div>
   </div>
 </template>
